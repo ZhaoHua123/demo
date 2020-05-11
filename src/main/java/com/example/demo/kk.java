a8db937168bd8916a079ee5316270e3160d95976
ZhaoHua123 <2858261073@qq.com>
2020-05-11 18:07:40 +0800

    zhoahua

--- src/main/java/com/example/demo/kk.java
+++ src/main/java/com/example/demo/kk.java
@@ -1,7 +1,15 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class kk {
     public static void main(String[] args) {
         System.out.println("12312312312321312321");
+    }
+    @RequestMapping("/hello")
+    public void hello(){
+
     }
 }
