6a40ee7c76359fc989350016f2498addd09cd263
ZhaoHua123 <65159349+ZhaoHua123@users.noreply.github.com>
2020-05-11 18:34:44 +0800

    Update controller.java

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class controller {
     public static void main(String[] args) {
         System.out.println("sdhkashdsjk  我成功了");
+ System.out.println("zhu");
     }
 }
